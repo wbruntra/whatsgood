c63a6a4dc228a65e16954177a605af92f89edb71
William Bruntrager <w.bruntrager@gmail.com>
2016-04-10 11:53:07 -0500

    heroku oauth callback

--- config.js
+++ config.js
@@ -51,7 +51,7 @@ var getConfig = module.exports = function () {
       clientId: process.env.OAUTH_CLIENT_ID || '1076675077133-qfuntqaqd96f4c1hl7c2ha5dnq2ls63b.apps.googleusercontent.com',
       clientSecret: process.env.OAUTH_CLIENT_SECRET || '1RFRRLormENgm7s9txCIy7LV',
       redirectUrl: process.env.OAUTH2_CALLBACK ||
-        'http://localhost:3000/oauth2callback',
+        'http://pacific-castle-63467.herokuapp.com/oauth2callback',
       scopes: ['email', 'profile']
     }
   };
